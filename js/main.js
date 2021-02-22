1e912da6a9f3f20144e4082e6c442e76e073462f
Koladele Olaitan <koladeleolaitan@gmail.com>
2021-02-22 14:07:46 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,47 +1,17 @@
-const courseList = document.querySelectorAll('.course-list label');
-const buttons = document.querySelectorAll('.desc-cont input');
+//fixed header 
+const header = document.querySelector('header');
+const navbarList = document.querySelectorAll('nav a');
+const lp = document.querySelector('.landing-page');
 
-const myFunc = ()=> {
-    courseList.forEach((course, index) => {
-        course.addEventListener('click', ()=> {
-            myFunc();
-            course.classList.add('.checked');
-            buttons.forEach((button, index)=> {
-                if(buttons[index].checked) {
-                    courseList[index].classList.remove('checked');
-                } else {
-                    course.classList.add('checked');
-                }
-            });
-        });
-        
+const slideObserverOptions = {};
+const slideObserver = new IntersectionObserver((entries, slideObserver) => {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            header.classList.add('fixed');
+        } else {
+            header.classList.remove('fixed');
+        }
     });
-};
+}, slideObserverOptions);
 
-window.onload = myFunc();
-
-
-//course slider on mobile
-let number = 1;
-setInterval(function(){
-    document.querySelector(`#slide${number}`).checked = true;
-    number++;
-    if (number > 4){
-        number=1;
-    }
-}, 4000);
-
-const form = document.querySelector('.contact-form');
-const overlay = document.querySelector('.overlay');
-const thanks = document.querySelector('.thanks');
-
-form.addEventListener('submit', (e)=> {
-    e.preventDefault();
-    overlay.classList.add('active');
-    thanks.classList.add('active');
-
-    overlay.addEventListener('click', ()=> {
-        overlay.classList.remove('active');
-        thanks.classList.remove('active');
-    });
-});
+slideObserver.observe(lp);
